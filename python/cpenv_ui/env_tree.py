2c32fa09adfd6fa71581b82fae29787401a1f932
Dan Bradham <danielbradham@gmail.com>
2020-08-12 12:16:35 -0400

    add: support for nested dictionaries to EnvTree

--- python/cpenv_ui/env_tree.py
+++ python/cpenv_ui/env_tree.py
@@ -4,7 +4,13 @@ from __future__ import print_function
 # Standard library imports
 import os
 
+try:
+    basestring
+except NameError:
+    basestring = (str, bytes)
+
 # Shotgun imports
+import sgtk
 from sgtk.platform.qt import QtCore, QtGui
 
 # PySide1 compat
@@ -12,6 +18,10 @@ try:
     QtGui.QHeaderView.setSectionResizeMode
 except AttributeError:
     QtGui.QHeaderView.setSectionResizeMode = QtGui.QHeaderView.setResizeMode
+
+
+app = sgtk.platform.current_bundle()
+
 
 # Tree Columns
 Key = 0
@@ -48,7 +58,45 @@ class EnvTree(QtGui.QTreeWidget):
         for k, v in sorted(data.items()):
             self.set(k, v)
 
-    def set(self, key, value):
+    def set(self, key, value, parent_item=None):
+        if os.pathsep in value:
+            value = value.split(os.pathsep)
+
+        if isinstance(value, basestring):
+            if parent_item:
+                child = QtGui.QTreeWidgetItem()
+                child.setText(Value, value)
+                if key != parent_item.text(Key):
+                    child.setText(Key, key)
+                parent_item.addChild(child)
+            else:
+                item = QtGui.QTreeWidgetItem(parent=self)
+                item.setText(Key, key)
+                item.setText(Value, value)
+                self.addTopLevelItem(item)
+        elif isinstance(value, (list, tuple)):
+            if not parent_item:
+                parent_item = QtGui.QTreeWidgetItem(parent=self)
+                parent_item.setText(Key, key)
+                self.addTopLevelItem(parent_item)
+
+            for v in value:
+                self.set(key, v, parent_item)
+        elif isinstance(value, dict):
+            if not parent_item:
+                parent_item = QtGui.QTreeWidgetItem(parent=self)
+                parent_item.setText(Key, key)
+                self.addTopLevelItem(parent_item)
+
+            for k, v in sorted(value.items()):
+                self.set(k, v, parent_item)
+        else:
+            app.info('Failed to display {}: {} in tree.'.format(k, v))
+
+        if parent_item:
+            parent_item.setExpanded(True)
+
+    def _set(self, key, value):
         self._data[key] = value
         if os.pathsep in value:
             parent = QtGui.QTreeWidgetItem(parent=self)
